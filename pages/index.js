3588d721078a832cce6fb7cd7dfcae9d9bdb911c
mgwall17 <mgwall17@gmail.com>
2021-12-03 16:21:42 -0700

    update

--- pages/index.js
+++ pages/index.js
@@ -36,8 +36,6 @@ export default function Home() {
       <ParallaxTwo />
       <div className="container-md">
         <OurTools />
-        <DEBlock />
-        <DataStoreDataCommons />
         <GettingStarted />
       </div>
       <footer>
